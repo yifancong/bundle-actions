173c9cfb738ef6151e325a4030df7240fbd8bb54
easy1090 <congzhe@bytedance.com>
2025-09-28 20:09:33 +0800

    chore: add actions publish workflow

--- rslib.config.ts
+++ rslib.config.ts
@@ -3,6 +3,11 @@ import { defineConfig } from '@rslib/core';
 export default defineConfig({
   lib: [
     {
+      output: {
+        distPath: {
+          root: './',
+        },
+      },
       bundle: true,
       dts: false,
       format: 'cjs',
